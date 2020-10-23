7f5316b498724c089fbf1e63f3ad6ea861ceff23
ericzw <ericwang13@protonmail.com>
2020-10-23 12:02:36 -0400

    Initialize ArrayOps.java and Tester.java

---
+++ ArrayOps.java
@@ -0,0 +1,22 @@
+public class ArrayOps {
+
+    public static int sum(int[] arr) {
+        return 0;
+    }
+
+    public static int largest(int[] arr) {
+        return 0;
+    }
+
+    public static int[] sumRows(int[][] matrix) {
+        return new int[0];
+    }
+
+    public static int[] largestInRows(int[][] matrix) {
+        return new int[0];
+    }
+
+    public static int sum(int[][] arr) {
+        return 0;
+    }
+}
